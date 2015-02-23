7ef6da4b680009a0cde5d0bab28c6a49e9170357
Yuichiro TCY <t.yic.yt@gmail.com>
2015-02-23 11:04:21 +0900

    Modify examples

---
+++ examples/js/plot.js
@@ -0,0 +1,14 @@
+// Sine data
+var x_min=-3, x_max=3, nbins = 60;
+var x = new Array(nbins), y = new Array(nbins);
+for (var i=0 ; i<nbins ; i++) {
+    x[i] = x_min + i*(x_max-x_min)/(nbins-1);
+    y[i] = Math.sin(x[i]);
+}
+
+// plot
+var plt = new Soba('#sine-curve');
+plt.plot(x,y,'b:');
+plt.xlabel('x');
+plt.ylabel('y');
+plt.show();
